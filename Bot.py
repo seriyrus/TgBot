d4451e29797eca76b1d663b7df5fa64f15adbe1a
seriyrus <123@test.ru>
2025-10-08 09:16:09 +0300

    init

--- Bot.py
+++ Bot.py
@@ -1,5 +1,6 @@
 import logging
 from aiogram import Bot, Dispatcher
+
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
